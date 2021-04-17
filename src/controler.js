5c14145775e78663a18c687e987fccaf98d25c3f
joaopauloqueiroz <joaopaulo.queiroz.545@gmail.com>
2021-04-17 13:51:36 -0300

    Add decode ecncript

--- src/controler.js
+++ src/controler.js
@@ -10,8 +10,9 @@ module.exports = {
 
   async get(req, res) {
     const { token } = req.query;
+    const buff = new Buffer(token, 'base64');
     try {
-      return res.send("Gerar link de pagamento para" + atob(token));
+      return res.send("Gerar link de pagamento para" + buff.toString('ascii'));
     } catch (error) {
       return res.status(500).send({ error: "Erro ao gerar link de pagamento" });
     }
