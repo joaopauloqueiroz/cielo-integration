aed5f1030bbb820885a9bc979b6037c7516259ec
joaopauloqueiroz <joaopaulo.queiroz.545@gmail.com>
2021-04-24 14:35:47 -0300

    Add test generate link itemm

--- src/controler.js
+++ src/controler.js
@@ -78,6 +78,7 @@ module.exports = {
         expirationDate: orderData.dateValidate
       }
       const response = await createLink(objectOrder, result.access_token)
+      console.log(response)
       return res.send(response.data);
      }
      return res.send("Orçamendo não e mais valido");
